c12e42be0a400ce6e8e32fb1cb59a3cefbae344d
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-21 10:05:32 -0300

    Tab_Bar Animated

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+import Journal from './src/pages/Journal';
+import Measures from './src/pages/Measures';
+import Treatment from './src/pages/Treatment';
+import Profile from './src/pages/Profile';
+import AddButton from './src/components/AddButton';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator tabBarOptions={{ showLabel: false}}> 
+        <Tab.Screen
+          name="Journal"
+          component={Journal}
+          options={{
+            tabBarIcon: () => <FontAwesome5 name="book-medical" size={24} color="#CDCCCE" />
+          }} />
+        <Tab.Screen
+          name="Measures"
+          component={Measures}
+          options={{
+            tabBarIcon: () => <FontAwesome5 name="heartbeat" size={24} color="#CDCCCE" />
+          }} />
+        <Tab.Screen
+          name="Add"
+          component={() => <AddButton />} 
+          options={{
+            tabBarIcon: () => <AddButton />
+          }} />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: () => <FontAwesome5 name="book-medical" size={24} color="#CDCCCE" />
+          }} />
+        <Tab.Screen
+          name="Treatment"
+          component={Treatment}
+          options={{
+            tabBarIcon: () => <FontAwesome5 name="book-medical" size={24} color="#CDCCCE" />
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Measures.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+export default function Measures(){
+  return (
+    <View style={{ flex: 1, backgroundColor: "#161924" }}>
+      <StatusBar barStyle="dark-content" />
+    </View>
+  )
+}
